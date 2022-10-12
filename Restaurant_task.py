d82c672e21ddb0ef9b84aedf96afe61bb0233b03
Faduma Muse <faduma@Fadumas-MacBook-Pro.local>
2022-10-12 17:57:19 +0100

    updated

---
+++ Restaurant_task.py
@@ -0,0 +1,19 @@
+
+# User Stories
+#1
+# AS a User I want to be able to see the menu in a formated way, so that I can order my meal.
+
+food_menu = {
+    "Starter" : ["Salad","Soup","Lamb chops"],
+    "main meals" : ["chicken & rice","Curry", "Chicken creamy pasta"],
+    "Drinks" : ["fanta","coke","water"],
+    "Dessert" : ["Chocolate cake", "Strawberry cake"]
+}
+
+print(food_menu)
+
+#2
+# AS a User I want to be able to order 3 times, and have my responses added to a list so they aren't forgotten
+
+order = []
+print(What would you like to order )
